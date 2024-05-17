6761eaa637964a14bc4da2fcd50f9d544a23822f
Shashwat <shashwat@Shashwats-MacBook-Pro-2.local>
2024-05-17 23:40:01 +0530

    Search implementation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import "./index.css"
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
 
   useEffect(()=>{
 fetch('https://restcountries.com/v3.1/all').then((res)=>res.json()).then((data)=>setCountries(data)).catch((error)=>console.log(error));
   },[])
   // console.log(countries)
+
+  const handleChange = (e) =>{
+setSearch(e.target.value)
+  }
+
+  useEffect(()=>{
+const resultVar = countries.filter(country=>country.name.common.toLowerCase().includes(search.toLowerCase()))
+setResult(resultVar);
+  },[search])
+
   return (
    <>
+   <div style={{width:'100vw', backgroundColor:'gray', display:'flex', justifyContent:'center', height:'36px'}}>
+    <input type="text" style={{width:'50vw', margin:'3px'}} placeholder="Search for Countries..." onChange={handleChange} value={search}/>
+   </div>
    <div className="container">
-   {countries.map((country)=><Countries img={country.flags.png} name={country.name.common}/>)}
+   {result.map((country)=><Countries img={country.flags.png} name={country.name.common}/>)}
    </div>
    </>
   );
--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import "./Countries.css"
 
 const Countries = ({img, name}) => {
-    console.log(name)
+    // console.log(name)
   return (
-    <div className='country'>
+    <div className='countryCard'>
         <img src={img} style={{width:'100px',height:'100px', alignSelf:'center'}} alt='flag'/>
         <div className='name'>{name}</div>
     </div>
