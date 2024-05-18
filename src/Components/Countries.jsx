e7e25d51d83e40366341122e8c835f520862dbad
Shashwat <shashwat@Shashwats-MacBook-Pro-2.local>
2024-05-18 15:40:24 +0530

    Test commit

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+// import React from 'react'
 import "./Countries.css"
 
-const Countries = ({img, name}) => {
-    // console.log(name)
-  return (
-    <div className='countryCard'>
-        <img src={img} style={{width:'100px',height:'100px', alignSelf:'center'}} alt='flag'/>
-        <div className='name'>{name}</div>
-    </div>
-  )
-}
+// const Countries = ({img, name}) => {
+//     // console.log(name)
+//   return (
+//     <div className='countryCard'>
+//         <img src={img} style={{width:'100px',height:'100px', alignSelf:'center'}} alt='flag'/>
+//         <div className='name'>{name}</div>
+//     </div>
+//   )
+// }
 
-export default Countries
+// export default Countries
+
+
+
+import React from "react";
+
+const CountriesSearch = ({ data }) => {
+  return (
+    <div className="countryCard">
+      <img src={data.flags.svg} alt={data.name.common} />
+      <h2>{data.name.common}</h2>
+    </div>
+  );
+};
+
+export default CountriesSearch;
