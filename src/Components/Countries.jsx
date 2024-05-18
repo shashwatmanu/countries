6f45955e4fc3301ec30fe59abb24f125df9c3d35
Shashwat <shashwat@Shashwats-MacBook-Pro-2.local>
2024-05-18 16:14:41 +0530

    Trial 2

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import "./Countries.css"
+// import React from 'react'
+// import "./Countries.css"
 
-const Countries = ({data}) => {
-    // console.log(name)
-  return (
-    <div className='countryCard'>
-        <img src={data.flags.svg} style={{width:'100px',height:'100px', alignSelf:'center'}} alt='flag'/>
-        <h2 className='name'>{data.name.common}</h2>
-    </div>
-  )
-}
-
-
-export default Countries
-
-
-
-// import React from "react";
-
-// const CountriesSearch = ({ data }) => {
+// const Countries = ({data}) => {
+//     // console.log(name)
 //   return (
-//     <div className="countryCard">
-//       <img src={data.flags.svg} alt={data.name.common} />
-//       <h2>{data.name.common}</h2>
+//     <div className='countryCard'>
+//         <img src={data.flags.svg} style={{width:'100px',height:'100px', alignSelf:'center'}} alt='flag'/>
+//         <h2 className='name'>{data.name.common}</h2>
 //     </div>
-//   );
-// };
+//   )
+// }
 
-// export default CountriesSearch;
+
+// export default Countries
+
+
+
+import React from "react";
+
+const CountriesSearch = ({ data }) => {
+  return (
+    <div className="countryCard">
+      <img src={data.flags.svg} alt={data.name.common} />
+      <h2>{data.name.common}</h2>
+    </div>
+  );
+};
+
+export default CountriesSearch;
